38363b366cef9ee26df09636c5e2d1a4aa30c9a2
Shnurok1342 <a13bepb@yandex.ru>
2021-03-02 19:58:49 +0300

    (lesson 42-44) feat: ControlValueAccessor for file-upload component

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,19 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, forwardRef, Input} from '@angular/core';
 import {HttpClient, HttpEventType} from '@angular/common/http';
 import {catchError, finalize} from 'rxjs/operators';
 import {of} from 'rxjs';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: 'file-upload.component.html',
-  styleUrls: ['file-upload.component.scss']
+  styleUrls: ['file-upload.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => FileUploadComponent),
+    multi: true
+  }]
 })
-export class FileUploadComponent {
+export class FileUploadComponent implements ControlValueAccessor {
   @Input() requiredFileType: string | string[];
 
   fileName = '';
   fileUploadError = false;
   uploadProgress: number;
+
+  disabled = false;
+  onChange = (fileName: string) => {};
+  onTouched = () => {};
 
   constructor(private http: HttpClient) {}
 
@@ -39,8 +49,31 @@ export class FileUploadComponent {
         .subscribe(e => {
           if (e.type === HttpEventType.UploadProgress) {
             this.uploadProgress = Math.round(100 * e.loaded / e.total);
+          } else if (e.type === HttpEventType.Response) {
+            this.onChange(this.fileName);
           }
         });
     }
+  }
+
+  onClick(fileInput: HTMLInputElement): void {
+    this.onTouched();
+    fileInput.click();
+  }
+
+  writeValue(value: string): void {
+    this.fileName = value;
+  }
+
+  registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 }
