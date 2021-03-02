ce29b24c13f036f558a04afc1dac4cf3bcd685da
Shnurok1342 <a13bepb@yandex.ru>
2021-03-02 16:24:52 +0300

    (lesson 26-27) feat: Building Multi-Step Reactive Form with Angular Material

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,18 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CoursesService} from '../../services/courses.service';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'create-course-step-1',
+  selector: 'app-create-course-step-1',
   templateUrl: './create-course-step-1.component.html',
   styleUrls: ['./create-course-step-1.component.scss']
 })
 export class CreateCourseStep1Component implements OnInit {
+  form = this.fb.group({
+    title: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(60)
+    ]]
+  });
 
-  ngOnInit() {
-
-  }
-
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {}
 }
