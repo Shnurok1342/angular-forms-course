277c65645584075ca8b92e3281a322d02f7617fb
Shnurok1342 <a13bepb@yandex.ru>
2021-03-02 19:28:17 +0300

    (lesson 37-40) feat: Custom form control - File Upload component

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,16 +1,35 @@
 import {Component, Input} from '@angular/core';
-import {HttpClient, HttpEventType} from '@angular/common/http';
-import {catchError, finalize} from 'rxjs/operators';
-import {AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator} from '@angular/forms';
-import {noop, of} from 'rxjs';
-
+import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {of} from 'rxjs';
 
 @Component({
-  selector: 'file-upload',
-  templateUrl: "file-upload.component.html",
-  styleUrls: ["file-upload.component.scss"]
+  selector: 'app-file-upload',
+  templateUrl: 'file-upload.component.html',
+  styleUrls: ['file-upload.component.scss']
 })
 export class FileUploadComponent {
+  @Input() requiredFileType: string | string[];
 
+  fileName = '';
+  fileUploadError = false;
 
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.fileName = file.name;
+      const formData = new FormData();
+      formData.append('thumbnail', file);
+      this.http.post('/api/thumbnail-upload', formData)
+        .pipe(
+          catchError(error => {
+            this.fileUploadError = true;
+            return of(error);
+          })
+        )
+        .subscribe();
+    }
+  }
 }
